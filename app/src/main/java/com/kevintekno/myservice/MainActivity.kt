e8dadc48e2f430f6e86310202180196e0753e584
Kevin Alfito <kevinalfito69@gmail.com>
2023-05-01 20:26:45 +0700

    Menambahkan Foreground Service

--- app/src/main/java/com/kevintekno/myservice/MainActivity.kt
+++ app/src/main/java/com/kevintekno/myservice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kevintekno.myservice
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kevintekno.myservice.databinding.ActivityMainBinding
@@ -12,8 +13,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        val foregroundServiceIntent = Intent(this, MyForegoundService::class.java)
         binding.btnStartBackgroundService.setOnClickListener { startService(serviceIntent) }
         binding.btnStopBackgroundService.setOnClickListener { stopService(serviceIntent) }
+
+        binding.btnStartForegroundService.setOnClickListener {onForegroundStart(foregroundServiceIntent) }
+        binding.btnStopForegroundService.setOnClickListener { onForegroundStop(foregroundServiceIntent) }
+    }
+    private fun onForegroundStart(foregroundServiceIntent:Intent){
+
+        if(Build.VERSION.SDK_INT >= 26){
+            startForegroundService(foregroundServiceIntent)
+        }else{
+            startService(foregroundServiceIntent)
+        }
+
+    }
+    private fun onForegroundStop(intent:Intent){
+        stopService(intent)
     }
 
 

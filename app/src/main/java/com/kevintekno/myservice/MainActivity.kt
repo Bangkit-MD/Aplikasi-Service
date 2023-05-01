020d695eec65d1face970bbb83dc804863a35348
Kevin Alfito <kevinalfito69@gmail.com>
2023-05-01 21:36:09 +0700

    Menambahkan Bound Service

--- app/src/main/java/com/kevintekno/myservice/MainActivity.kt
+++ app/src/main/java/com/kevintekno/myservice/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.kevintekno.myservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.kevintekno.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +17,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val serviceIntent = Intent(this, MyBackgroundService::class.java)
         val foregroundServiceIntent = Intent(this, MyForegoundService::class.java)
+        val boundServiceIntent = Intent(this, MyBoundService::class.java)
         binding.btnStartBackgroundService.setOnClickListener { startService(serviceIntent) }
         binding.btnStopBackgroundService.setOnClickListener { stopService(serviceIntent) }
 
         binding.btnStartForegroundService.setOnClickListener {onForegroundStart(foregroundServiceIntent) }
         binding.btnStopForegroundService.setOnClickListener { onForegroundStop(foregroundServiceIntent) }
+
+        binding.btnStartBoundService.setOnClickListener {
+            bindService(boundServiceIntent, connection, BIND_AUTO_CREATE)
+        }
+        binding.btnStopBoundService.setOnClickListener {
+            unbindService(connection)
+        }
+
+    }
+    private var boundStatus = false
+    private lateinit var boundService: MyBoundService
+
+    private var connection = object : ServiceConnection {
+
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            boundService = myBinder.getService
+            boundStatus= true
+            getNumberDataFromService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            boundStatus= false
+        }
+    }
+    private fun getNumberDataFromService(){
+        boundService.numberLiveData.observe(this){number ->
+            binding.tvBoundServiceNumber.text = number.toString()
+
+        }
     }
     private fun onForegroundStart(foregroundServiceIntent:Intent){
 
@@ -31,6 +66,14 @@ class MainActivity : AppCompatActivity() {
     }
     private fun onForegroundStop(intent:Intent){
         stopService(intent)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(boundStatus){
+            unbindService(connection)
+            boundStatus = false
+        }
     }
 
 
